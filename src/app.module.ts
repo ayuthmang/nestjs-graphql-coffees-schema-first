828edc1e58fa929296626dc6531703ed830c69d7
Ayuth Mangmesap <ayuth.mang@gmail.com>
2023-10-14 16:21:24 +0700

    Getting Started - Creating our first GraphQL Application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver, // 👈 Using the ApolloDriver
+      typePaths: ['./**/*.graphql'], // 👈 where our (.)graphql files are located
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
